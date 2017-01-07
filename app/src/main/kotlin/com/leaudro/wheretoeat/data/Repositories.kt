5c8436ea082e291840bad1f5ed591aab0c92a9c6
Leandro Santos <leaudro@gmail.com>
2017-01-07 19:07:05 -0300

    Add fields to Place Model

--- app/src/main/kotlin/com/leaudro/wheretoeat/data/Repositories.kt
+++ app/src/main/kotlin/com/leaudro/wheretoeat/data/Repositories.kt
@@ -3,6 +3,10 @@ package com.leaudro.wheretoeat.data
 import com.leaudro.wheretoeat.data.model.Place
 
 class PlacesRepository : PlacesDataSource {
-    override fun getPlaces() = emptyList<Place>()
+    override fun getPlaces(): List<Place> {
+        return listOf(Place(0, "Restaurante 1", "Restaurante de comida baiana com ótimos aperitivos. Promoção toda sexta"),
+                Place(1, "Restaurante 2", "Restaurante de comida baiana com ótimos aperitivos. Promoção toda sexta"),
+                Place(2, "Restaurante 3", "Restaurante\n de comida baiana\n com ótimos aperitivos. \nPromoção toda sexta"))
+    }
 
 }
--- app/src/main/kotlin/com/leaudro/wheretoeat/data/model/Place.kt
+++ app/src/main/kotlin/com/leaudro/wheretoeat/data/model/Place.kt
@@ -1,5 +1,9 @@
 package com.leaudro.wheretoeat.data.model
 
-data class Place(val id: Long) {
+data class Place(val id: Long,
+                 val name: String,
+                 val description: String) {
+
+    constructor(id: Long): this(id, "", "")
 
 }
