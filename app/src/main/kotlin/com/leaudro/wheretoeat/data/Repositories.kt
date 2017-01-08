f66127b8407c22912a546916a159abeffee0d954
Leandro Santos <leaudro@gmail.com>
2017-01-08 12:56:27 -0300

    Add fields to Place model and update repository

--- app/src/main/kotlin/com/leaudro/wheretoeat/data/Repositories.kt
+++ app/src/main/kotlin/com/leaudro/wheretoeat/data/Repositories.kt
@@ -6,12 +6,13 @@ import rx.lang.kotlin.toSingletonObservable
 import java.util.concurrent.TimeUnit
 
 class PlacesRepository : PlacesDataSource {
-        private val placesList = listOf(Place(0, "Restaurante 1", "Restaurante de comida baiana com ótimos aperitivos. Promoção toda sexta"),
-                Place(1, "Restaurante 2", "Restaurante de comida baiana com ótimos aperitivos. Promoção toda sexta"),
-                Place(2, "Restaurante 3", "Restaurante\n de comida baiana\n com ótimos aperitivos. \nPromoção toda sexta"))
+        private val placesList = listOf(Place(0, "Restaurante 1", "Restaurante de comida baiana com ótimos aperitivos. Promoção toda sexta", 3, false, false),
+                Place(1, "Restaurante 2", "Restaurante de comida baiana com ótimos aperitivos. Promoção toda sexta", 0, false, true),
+                Place(2, "Restaurante 3", "Restaurante\n de comida baiana\n com ótimos aperitivos. \nPromoção toda sexta", 1, false, false),
+                Place(3, "Restaurante 4", "Restaurante de comida baiana com ótimos aperitivos. \nPromoção toda sexta", 6, false, false),
+                Place(4, "Restaurante 5", "Restaurante", 0, false, true))
 
     override fun getPlaces(): Observable<List<Place>> {
         return placesList.toSingletonObservable().delay(5, TimeUnit.SECONDS)
     }
-
 }
--- app/src/main/kotlin/com/leaudro/wheretoeat/data/model/Place.kt
+++ app/src/main/kotlin/com/leaudro/wheretoeat/data/model/Place.kt
@@ -2,8 +2,11 @@ package com.leaudro.wheretoeat.data.model
 
 data class Place(val id: Long,
                  val name: String,
-                 val description: String) {
+                 val description: String,
+                 val votesReceived: Int,
+                 val votedByYou: Boolean,
+                 val chosenThisWeek: Boolean) {
 
-    constructor(id: Long): this(id, "", "")
+    constructor(id: Long): this(id, "", "",0, false, false)
 
 }
